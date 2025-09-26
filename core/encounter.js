f1da9ecf91951d57b71a89d7410d32729717b834
Patobeur <patobeur41@gmail.com>
2025-09-26 08:55:07 +0200

    new file:   core/encounter.js 	new file:   core/fight.js 	modified:   core/main.js 	new file:   core/rest.js 	modified:   index.html

---
+++ core/encounter.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const encounter = {
+  start: function() {
+    front.displayStepInfo();
+    front.setStepBackgroundImage();
+
+    addMessage('Vous rencontrez : ' + player.event.name + '.');
+
+    front.displayMarket();
+
+    // front.stepBoardDiv.appendChild(front.nextButton);
+    front.displayNextStepButton();
+    player.refreshPlayerStats();
+  }
+};
--- core/main.js
+++ core/main.js
@@ -14,7 +14,7 @@ const game = {
       this.nextStep();
     });
     front.discardButton.addEventListener("click",()=>{cartes.discardCards()});
-    front.attackButton.addEventListener("click",()=>{this.playerAttack()});
+    front.attackButton.addEventListener("click",()=>{fight.playerAttack()});
   },
 
   nextStep:function() {
@@ -32,92 +32,10 @@ const game = {
     console.log('eventType:',player.eventType)
     console.table(player.event)
 
-    if(player.eventType=='fight'){this.startFight();}
-    if(player.eventType=='rest'){this.startRest();}
-    if(player.eventType=='encounter'){this.startEncounter(player.step);}
+    if(player.eventType=='fight'){fight.start();}
+    if(player.eventType=='rest'){rest.start();}
+    if(player.eventType=='encounter'){encounter.start();}
     if(player.eventType=='win'){this.gameOver(true);}
-  },
-
-  startRest:function () {
-    console.log('--------startRest---------')
-    front.displayStepInfo()
-    front.setStepBackgroundImage()
-
-    front.deckActions.style.display = "none";
-
-    addMessage(player.event.name);
-
-    if(player.event.bonus){
-      let message = player.setRestBonus()
-      addMessage(message ?? 'vide');
-    }
-
-    front.displayNextStepButton()
-    player.refreshPlayerStats();
-  },
-  startFight:function() {
-    enemys.set(player.event);
-    addMessage(player.event.accroche ?? `Vous affrontez ${enemys.current.prefixed}`);
-
-    front.displayStepInfo()
-    front.setStepBackgroundImage()
-
-    player.refreshPlayerStats();
-    cartes.dealCards();
-  },
-
-  enemyTurn:function() {
-    enemys.attack();
-
-    if (player.values.hp <= 0) {
-      addMessage("Vous avez été vaincu ! ");
-      gameOver();
-    } else {
-      front.refreshEnemyStats();
-      player.refreshPlayerStats();
-    }
-  },
-  playerAttack: function(){
-    if (cartes.selectedCards.length === 0) return;
-    player.values.plis += 1;
-
-    const check = cartes.checkCombinations(cartes.selectedCards);
-    console.log('--------playerAttack--------')
-    enemys.takeDamage(check.points);
-    front.refreshEnemyStats();
-
-    addMessage(`Vous attaquez l'ennemi et lui infligez ${check.points} dégâts avec ${check.hand} !`);
-
-    cartes.discardCards();
-    if (enemys.current && enemys.current.stats.hp.cur <= 0) {
-      addMessage(`Vous avez vaincu ${enemys.current.prefixed}!`);
-      // rewards
-      if(player.event.reward){
-        let message = player.setRewardBonus(player.event.reward)
-        addMessage(message ?? 'vide');
-      }
-      cartes.hideDeck()
-      front.displayNextStepButton()
-
-
-    } else {
-      this.enemyTurn();
-    }
-  },
-  startEncounter:function() {
-    front.displayStepInfo()
-    front.setStepBackgroundImage()
-
-    addMessage('Vous rencontrez : '+player.event.name+'.');
-
-
-      front.displayMarket()
-
-
-    // front.stepBoardDiv.appendChild(front.nextButton);
-    front.displayNextStepButton()
-    player.refreshPlayerStats();
-
   }
 }
 
